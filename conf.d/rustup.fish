4b1261a91da610b8348793627d1a68d8fada08d2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2026-06-02 11:42:21 +0200

    Use brew rustup keg when available

--- conf.d/rustup.fish
+++ conf.d/rustup.fish
@@ -1,7 +1,17 @@
-set -l rustup_path $HOME/.cargo/bin
+set -l rustup_path
 
-if [ $CARGO_HOME ]
-  set rustup_path $CARGO_HOME/bin
+if type -q brew
+  set -l brew_prefix (brew --prefix rustup 2>/dev/null)
+  if test -n "$brew_prefix" -a -d "$brew_prefix/bin"
+    set rustup_path "$brew_prefix/bin"
+  end
+end
+
+if test -z "$rustup_path"
+  set rustup_path $HOME/.cargo/bin
+  if [ $CARGO_HOME ]
+    set rustup_path $CARGO_HOME/bin
+  end
 end
 
 contains -- $rustup_path $PATH
