56130aac0caf4b5ad2f571d11e4318051d718195
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2026-06-30 20:59:04 +0200

    Always add cargo bin to PATH, brew keg additionally

--- conf.d/rustup.fish
+++ conf.d/rustup.fish
@@ -1,18 +1,22 @@
-set -l rustup_path
+set -l cargo_bin $HOME/.cargo/bin
+
+if [ $CARGO_HOME ]
+  set cargo_bin $CARGO_HOME/bin
+end
+
+set -l keg_bin
 
 if type -q brew
   set -l brew_prefix (brew --prefix rustup 2>/dev/null)
   if test -n "$brew_prefix" -a -d "$brew_prefix/bin"
-    set rustup_path "$brew_prefix/bin"
+    set keg_bin "$brew_prefix/bin"
   end
 end
 
-if test -z "$rustup_path"
-  set rustup_path $HOME/.cargo/bin
-  if [ $CARGO_HOME ]
-    set rustup_path $CARGO_HOME/bin
-  end
-end
+contains -- $cargo_bin $PATH
+  or set -gx PATH $cargo_bin $PATH
 
-contains -- $rustup_path $PATH
-  or set -gx PATH $rustup_path $PATH
+if test -n "$keg_bin"
+  contains -- $keg_bin $PATH
+    or set -gx PATH $keg_bin $PATH
+end
